e74fc5c10ad9acd9de27c0380b4ec0ff29b74666
Alexandre Fonseca <alexandrefonseca@live.com>
2022-02-03 18:51:37 +0000

    Format through black

--- autostatic.py
+++ autostatic.py
@@ -38,7 +38,9 @@ PELICAN_LIKE_HREF_PATTERN = r"""
 
     (?P<quote>["\'])      # require value to be quoted
     (?P<ref>{0}(?P<path>.*?))  # the url value
-    \2""".format(PELICAN_LIKE_REF_TAG)
+    \2""".format(
+    PELICAN_LIKE_REF_TAG
+)
 
 
 def html_unescape(text):
@@ -58,27 +60,32 @@ def parse_static_references(instance, text):
         using_relative_urls = instance._context.get("RELATIVE_URLS")
 
         if extra:
-            for match in re.finditer(r'(\w+)="?((?:(?<!")[^\s|]+|(?<=")(?:\\.|[^"\\])*(?=")))"?', extra):
+            for match in re.finditer(
+                r'(\w+)="?((?:(?<!")[^\s|]+|(?<=")(?:\\.|[^"\\])*(?=")))"?', extra
+            ):
                 extra_dict[match.group(1)] = match.group(2)
 
-        if path.startswith('/'):
+        if path.startswith("/"):
             source_path = path[1:]
             destination_path = source_path
             relative_path = False
         else:
             source_path = instance.get_relative_source_path(
-                os.path.join(instance.relative_dir, path))
+                os.path.join(instance.relative_dir, path)
+            )
             destination_path = os.path.join(instance_destination_dir, path)
             relative_path = True
 
         if "output" in extra_dict:
             output_override = extra_dict["output"]
 
-            if output_override.startswith('/'):
+            if output_override.startswith("/"):
                 destination_path = output_override[1:]
                 relative_path = False
             else:
-                destination_path = os.path.join(instance_destination_dir, output_override)
+                destination_path = os.path.join(
+                    instance_destination_dir, output_override
+                )
                 relative_path = True
 
         if using_relative_urls:
@@ -94,7 +101,7 @@ def parse_static_references(instance, text):
         if "url" in extra_dict:
             url_override = extra_dict["url"]
 
-            if url_override.startswith('/'):
+            if url_override.startswith("/"):
                 url = siteurl + url_override
             else:
                 url = url_override
@@ -102,26 +109,25 @@ def parse_static_references(instance, text):
                 if not using_relative_urls:
                     url = siteurl + "/" + os.path.dirname(instance.save_as) + "/" + url
 
-        url = url.replace('\\', '/')  # for Windows paths.
+        url = url.replace("\\", "/")  # for Windows paths.
 
         static_path_obj = StaticPath(source_path, destination_path, url, extra_dict)
         autostatic_path_found.send(autostatic_path=static_path_obj)
 
-        logger.debug("Detected autostatic path: {} -> {} ({})".format(
-            static_path_obj.source,
-            static_path_obj.destination,
-            static_path_obj.url))
+        logger.debug(
+            "Detected autostatic path: {} -> {} ({})".format(
+                static_path_obj.source, static_path_obj.destination, static_path_obj.url
+            )
+        )
 
         detected_autostatic_paths[static_path_obj.destination] = static_path_obj.source
 
         return html_escape(static_path_obj.url)
 
-
     def _parse_pelican_like_reference(m):
-        return ''.join((m.group('markup'), m.group('quote'),
-                        _get_static_path(m),
-                        m.group('quote')))
-
+        return "".join(
+            (m.group("markup"), m.group("quote"), _get_static_path(m), m.group("quote"))
+        )
 
     if text is None:
         return text
@@ -137,8 +143,12 @@ def parse_static_references(instance, text):
         text = re.sub(static_ref_re_pattern, _get_static_path, text)
 
         if settings and settings.get(USE_PELICAN_LIKE_REF_KEY, False):
-            text = re.sub(PELICAN_LIKE_HREF_PATTERN,
-                          _parse_pelican_like_reference, text, flags=re.X)
+            text = re.sub(
+                PELICAN_LIKE_HREF_PATTERN,
+                _parse_pelican_like_reference,
+                text,
+                flags=re.X,
+            )
 
         return text
     elif isinstance(text, list):
@@ -198,37 +208,41 @@ class AutoStaticGenerator(generators.Generator):
     autostatic_generator_context = signal("autostatic_generator_context")
     autostatic_generator_finalized = signal("autostatic_generator_finalized")
 
-
     def __init__(self, *args, **kwargs):
         super(AutoStaticGenerator, self).__init__(*args, **kwargs)
         self.autostatic_files = []
         self.autostatic_generator_init.send(self)
 
-
     def add_static_path(self, source_path, save_as):
         try:
             static = self.readers.read_file(
-                    base_path=self.path, path=source_path, content_class=Static,
-                    fmt='static', context=self.context,
-                    preread_signal=self.autostatic_generator_preread,
-                    preread_sender=self,
-                    context_signal=self.autostatic_generator_context,
-                    context_sender=self)
+                base_path=self.path,
+                path=source_path,
+                content_class=Static,
+                fmt="static",
+                context=self.context,
+                preread_signal=self.autostatic_generator_preread,
+                preread_sender=self,
+                context_signal=self.autostatic_generator_context,
+                context_sender=self,
+            )
             static.override_save_as = save_as
             self.autostatic_files.append(static)
             self.add_source_path(static)
         except Exception as e:
-            logger.error("Could not process %s\n%s", source_path, e,
-                    exc_info=self.settings.get("DEBUG", False))
-
+            logger.error(
+                "Could not process %s\n%s",
+                source_path,
+                e,
+                exc_info=self.settings.get("DEBUG", False),
+            )
 
     def finalize_context(self):
         for save_as, source_path in detected_autostatic_paths.items():
             self.add_static_path(source_path, save_as)
 
-        self._update_context(('autostatic_files',))
+        self._update_context(("autostatic_files",))
         self.autostatic_generator_finalized.send(self)
-
 
     def generate_output(self, writer):
         # copy all Static files
@@ -237,7 +251,7 @@ class AutoStaticGenerator(generators.Generator):
             save_as = os.path.join(self.output_path, sc.save_as)
             mkdir_p(os.path.dirname(save_as))
             shutil.copy2(source_path, save_as)
-            logger.info('Copying %s to %s', sc.source_path, sc.save_as)
+            logger.info("Copying %s to %s", sc.source_path, sc.save_as)
 
 
 def find_static_references(instance):
@@ -247,10 +261,16 @@ def find_static_references(instance):
         instance._summary = parse_static_references(instance, instance._summary)
 
     for key in instance.metadata.keys():
-        instance.metadata[key] = parse_static_references(instance, instance.metadata[key])
+        instance.metadata[key] = parse_static_references(
+            instance, instance.metadata[key]
+        )
         try:
-            setattr(instance, key.lower(), parse_static_references(instance, getattr(instance, key.lower())))
-        except AttributeError as e:
+            setattr(
+                instance,
+                key.lower(),
+                parse_static_references(instance, getattr(instance, key.lower())),
+            )
+        except AttributeError:
             pass
 
 
@@ -271,5 +291,7 @@ def generators_finished(_):
 def register():
     signals.content_object_init.connect(find_static_references)
     signals.get_generators.connect(get_generators)
-    AutoStaticGenerator.autostatic_generator_init.connect(autostatic_generator_initialized)
+    AutoStaticGenerator.autostatic_generator_init.connect(
+        autostatic_generator_initialized
+    )
     signals.get_writer.connect(generators_finished)
--- setup.py
+++ setup.py
@@ -1,30 +1,27 @@
 from setuptools import setup
 
-with open('README.rst') as f:
+with open("README.rst") as f:
     long_description = f.read()
 
 setup(
-
     # Basic package information:
-    name = 'pelican-autostatic',
-    version = '1.0.0',
-    py_modules = ('autostatic',),
-
+    name="pelican-autostatic",
+    version="1.0.0",
+    py_modules=("autostatic",),
     # Packaging options:
-    zip_safe = False,
-    include_package_data = True,
-
+    zip_safe=False,
+    include_package_data=True,
     # Package dependencies:
-    install_requires = ['pelican>=3.4.0'],
-
+    install_requires=["pelican>=3.4.0"],
     # Metadata for PyPI:
-    author = 'Alexandre Fonseca',
-    author_email = 'alexandrejorgefonseca@gmail.com',
-    license = 'Apache',
-    url = 'https://github.com/AlexJF/pelican-autostatic',
-    download_url = 'https://github.com/AlexJF/pelican-autostatic/archive/v0.2.2.zip',
-    keywords = 'pelican blog static generic automatic',
-    description = ('A generator for Pelican allowing flexible referencing '
-            'of static content'),
-    long_description = long_description
+    author="Alexandre Fonseca",
+    author_email="alexandrejorgefonseca@gmail.com",
+    license="Apache",
+    url="https://github.com/AlexJF/pelican-autostatic",
+    download_url="https://github.com/AlexJF/pelican-autostatic/archive/v0.2.2.zip",
+    keywords="pelican blog static generic automatic",
+    description=(
+        "A generator for Pelican allowing flexible referencing " "of static content"
+    ),
+    long_description=long_description,
 )
